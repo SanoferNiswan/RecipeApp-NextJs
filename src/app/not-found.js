61c98b3ddae2a19e1d2e4ed24b004d2095bbf41d
sanoferniswan <sanoferniswan786@gmail.com>
2024-12-26 23:24:13 +0530

    style added

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,16 @@
-import Link from "next/link"
-export default function NotFound(){
-    return <div>
-        <h1>Page not found</h1>
-        <Link href={'/'}>Go to home page</Link>
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="text-center p-8 rounded-lg w-4/5 sm:w-1/2 md:w-1/3">
+        <h1 className="text-4xl font-bold text-gray-700 mb-4">Page Not Found</h1>
+        <p className="text-lg text-gray-500 mb-6">
+          Oops! It seems the page you're looking for doesn't exist.
+        </p>
+        <Link href="/" className="px-6 py-3 bg-purple-500 text-white font-semibold rounded-md hover:bg-purple-600 transition duration-200">Go to Home Page
+        </Link>
+      </div>
     </div>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-b from-gray-100 to-gray-200">
+    <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 sm:p-20 bg-gradient-to-b from-gray-100 to-gray-200">
       <div className="text-center">
         <h1 className="text-6xl font-extrabold text-gray-900 mb-6 drop-shadow-lg">
           Welcome to Recipe App
@@ -14,7 +14,7 @@ export default function Home() {
       
       <div className="mt-12">
         <Link href={'/recipe-list'}>
-          <button className="bg-blue-600 text-white text-xl font-semibold py-4 px-10 rounded-lg shadow-lg hover:bg-blue-700 transition-all duration-300">
+          <button className="bg-purple-600 text-white text-xl font-semibold py-4 px-10 rounded-lg shadow-lg hover:bg-purple-700 transition-all duration-300">
             Explore All Recipes
           </button>
         </Link>
